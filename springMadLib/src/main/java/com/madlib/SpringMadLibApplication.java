419f93a1a00e3c762b93546c13c17d2b52cede3e
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-07-09 10:20:10 -0400

    initial commit

---
+++ springMadLib/src/main/java/com/madlib/SpringMadLibApplication.java
@@ -0,0 +1,13 @@
+package com.madlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMadLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMadLibApplication.class, args);
+	}
+
+}
---
+++ springMadLib/src/test/java/com/madlib/SpringMadLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMadLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
